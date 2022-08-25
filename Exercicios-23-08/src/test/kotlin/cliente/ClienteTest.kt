b842a38d80a6c80419c37e666cd903237e4bf064
Michell Platini <michell_platini21@hotmail.com>
2022-08-25 16:48:04 -0300

    Adicionando testes

--- Exercicios-23-08/src/test/kotlin/cliente/ClienteTest.kt
+++ Exercicios-23-08/src/test/kotlin/cliente/ClienteTest.kt
@@ -3,14 +3,29 @@ package cliente
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 
-internal class ClienteTest {
+class ClienteTest {
+
+    val cliente = Cliente(nome = "Michell", fone= "123456", end= "rua das couves")
 
     @Test
-    fun insereItem() {
-    }
 
+    fun insereItem() {
+
+        cliente.insereItem("arroz")
+        cliente.insereItem("feijão")
+
+        assertEquals(2,cliente.list.size)
+        assertTrue(cliente.list.contains("arroz"))
+    }
+}
+
+
+    /*
     @Test
     fun excluirItem() {
     }
 }
+
+     */
